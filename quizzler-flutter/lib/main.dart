00adc5295321dd2ac985f5a7344180f3f398ed1a
rookie <dnwlsskfn@gmail.com>
2021-01-18 14:55:13 +0900

    just

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -6,6 +9,7 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -25,58 +29,6 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [];
-
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-
-  List<bool> answer = [
-    false, true, true
-  ];
-
-  int questionNumber = 0;
-
-  void checkTrue(int questionNumber){
-    if(answer[questionNumber]==true) {
-      scoreKeeper.add(
-        Icon(
-          Icons.check,
-          color: Colors.green,
-        ),
-      );
-    }
-    else{
-      scoreKeeper.add(
-        Icon(
-          Icons.close,
-          color: Colors.red,
-        ),
-      );
-    }
-
-  }
-  void checkFalse(int questionNumber){
-    if(answer[questionNumber]==false) {
-      scoreKeeper.add(
-        Icon(
-          Icons.check,
-          color: Colors.green,
-        ),
-      );
-    }
-    else{
-      scoreKeeper.add(
-        Icon(
-          Icons.close,
-          color: Colors.red,
-        ),
-      );
-    }
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.questionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -115,8 +67,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  checkTrue(questionNumber);
-                  questionNumber+=1;
+                  quizBrain.checkTrue();
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -136,14 +88,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  checkFalse(questionNumber);
-                  questionNumber+=1;
+                  quizBrain.checkFalse();
+                  quizBrain.nextQuestion();
                 });
               },
             ),
           ),
         ),
-        Row(children: scoreKeeper),
+        Row(children: quizBrain.scoreKeeper),
       ],
     );
   }
---
+++ quizzler-flutter/lib/quiz_brain.dart
@@ -0,0 +1,107 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'question.dart';
+
+
+class QuizBrain {
+  int _questionNumber = 0;
+  List<Widget> scoreKeeper = [];
+
+  List<Question> _questionAll = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  void nextQuestion(){
+    if(_questionNumber<_questionAll.length-1) _questionNumber++;
+    else{
+      finish();
+    }
+  }
+
+  Widget finish(){
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: Colors.blue,
+      child: Text(
+        'Finish',
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+    );
+  }
+
+  void checkTrue(){
+    if(_questionAll[_questionNumber].questionAnswer==true) {
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    }
+    else{
+      scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+
+  }
+  void checkFalse(){
+    if(_questionAll[_questionNumber].questionAnswer==false) {
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    }
+    else{
+      scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+
+  }
+
+  String questionText(){
+    return _questionAll[_questionNumber].questionText;
+  }
+
+  bool questionAnswer(){
+    return _questionAll[_questionNumber].questionAnswer;
+  }
+
+}
