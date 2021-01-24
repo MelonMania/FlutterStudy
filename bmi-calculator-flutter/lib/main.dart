4223511e6b8729e5bb64ba3610b8d2198a553d53
rookie <dnwlsskfn@gmail.com>
2021-01-25 01:36:38 +0900

    debugBanner close

--- bmi-calculator-flutter/lib/main.dart
+++ bmi-calculator-flutter/lib/main.dart
@@ -9,6 +9,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0D22),
 
