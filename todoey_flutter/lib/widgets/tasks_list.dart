932fea99562be1bd4e3f38d57afaf63065dc30d9
MelonMania <dnwlsskfn@gmail.com>
2021-02-13 15:13:47 +0900

    function complete

--- todoey_flutter/lib/widgets/tasks_list.dart
+++ todoey_flutter/lib/widgets/tasks_list.dart
@@ -5,29 +5,29 @@ import 'package:todoey_flutter/widgets/task_tile.dart';
 import 'package:todoey_flutter/screens/add_task_screen.dart';
 
 class TileList extends StatefulWidget {
+  final List<Task> taskList;
+
+  TileList(this.taskList);
+
   @override
   _TileListState createState() => _TileListState();
 }
 
 class _TileListState extends State<TileList> {
-  List<Task> taskList = [
-    Task(title: 'aaa'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemBuilder: (context, index) {
           return TaskTile(
-            title: taskList[index].title,
-            isChecked: taskList[index].isDone,
+            title: widget.taskList[index].title,
+            isChecked: widget.taskList[index].isDone,
             checkboxCallback: (bool value) {
               setState(() {
-                taskList[index].toggleDone();
+                widget.taskList[index].toggleDone();
               });
             },
           );
         },
-        itemCount: taskList.length);
+        itemCount: widget.taskList.length);
   }
 }
